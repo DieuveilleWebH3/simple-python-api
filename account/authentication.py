bab818f8732bcb5ca8636f61cfffb47c963cfbc9
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-06-21 15:11:50 +0200

    OK, created new app account, not used for now, ..

---
+++ account/authentication.py
@@ -0,0 +1,33 @@
+
+"""
+
+    Created on Monday, June 21 2022  14:37:54 
+    
+    @author: Dieuveille BOUSSA ELLENGA
+    
+"""
+
+
+#from django.contrib.auth.models import User
+from .models import *
+
+
+class EmailAuthBackend(object):
+    """
+    Authenticate using an e-mail address.
+    """
+    def authenticate(self, request, username=None, password=None):
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
---
+++ account/tests.py
@@ -0,0 +1,13 @@
+
+"""
+
+    Created on Monday, January 24 2022  15:37:54 
+    
+    @author: Dieuveille BOUSSA ELLENGA
+    
+"""
+
+
+from django.test import TestCase
+
+# Create your tests here.
