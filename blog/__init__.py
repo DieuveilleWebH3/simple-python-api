7b6eafe9ecdef46426d0dfa8cdc13342d1bf1ea1
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-06-28 11:33:39 +0200

    OK: initialized blog app, adding models now, ..

---
+++ blog/__init__.py
@@ -0,0 +1,8 @@
+
+"""
+
+    Created on Tuesday, June 28 2022  11:26:54 
+    
+    @author: Dieuveille BOUSSA ELLENGA
+    
+"""
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,21 @@
-from django.db import models
+
+"""
+
+    Created on Tuesday, June 28 2022  11:26:54 
+    
+    @author: Dieuveille BOUSSA ELLENGA
+    
+"""
+
+
+from django.db import models 
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Group
+import os
+
 
 # Create your models here.
+
+
 
