1a671e2622d2a4f563f2489e41d752635631ee76
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-06-28 15:49:08 +0200

    OK : added Category Model into admin blog template, ..

--- blog/admin.py
+++ blog/admin.py
@@ -8,5 +8,17 @@
 """
 
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import *
+
 
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)} 
+    list_display = ['id','title','slug', "created_at", "modified_at"] 
+    search_fields = ['title', 'id' ] 
+    filter_fields = ['title', 'slug'] 
+
