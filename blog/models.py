3b87949742a0cd3ce26d7cd8217cdbbeeda38cf4
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-06-28 11:56:08 +0200

    OK : added Category Model, onto the next, ..

--- blog/models.py
+++ blog/models.py
@@ -17,5 +17,30 @@ import os
 
 # Create your models here.
 
+# Model Axe 
+class Category(models.Model):
+    title = models.CharField(max_length=255, unique=True, blank=False, null=False)
+    slug = models.CharField(max_length=255, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+        
+    def __str__(self):
+        return self.title
+
+
+# Model Theme 
+class Theme(models.Model):
+    axe = models.ForeignKey(Axe, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, unique=False, blank=False, null=False)
+    slug = models.CharField(max_length=255, unique=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+        
+    def __str__(self):
+        return f'{str(self.title)} - {str(self.axe)}'
+
+
 
 
